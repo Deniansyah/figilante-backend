9084a1d77ee9e76f456cfb791951f95f3b1cf860
Rio Prayoga <yoprayogap@gmail.com>
2022-12-31 14:36:08 +0700

    fix CRUD categories

--- src/controllers/categories.controller.js
+++ src/controllers/categories.controller.js
@@ -17,3 +17,110 @@ exports.getAllCategories = async (req, res) => {
     });
   }
 };
+
+exports.getCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const category = await prisma.categories.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+    });
+    if (!category) {
+      return res.status(404).json({
+        success: false,
+        message: `category with id ${id} not found`,
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: `category with id ${id} found`,
+      results: category,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+exports.createCategory = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const category = await prisma.categories.create({
+      data: {
+        name,
+      },
+    });
+    return res.status(201).json({
+      success: true,
+      message: "Category created successfully",
+      results: category,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+exports.updateCategory = async (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  try {
+    const category = await prisma.categories.update({
+      where: {
+        id: parseInt(id),
+      },
+      data: {
+        name,
+      },
+    });
+    return res.status(200).json({
+      success: true,
+      message: `Category with id ${id} updated successfully`,
+      results: category,
+    });
+  } catch (error) {
+    console.log(error);
+    if (error.code === "P2025") {
+      return res.status(404).json({
+        success: false,
+        message: `Category with id ${id} not found`,
+      });
+    }
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+exports.deleteCategory = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const category = await prisma.categories.delete({
+      where: {
+        id: parseInt(id),
+      },
+    });
+    return res.status(200).json({
+      success: true,
+      message: `Category with id ${id} deleted successfully`,
+      results: category,
+    });
+  } catch (error) {
+    if (error.code === "P2025") {
+      return res.status(404).json({
+        success: false,
+        message: `Category with id ${id} not found`,
+      });
+    }
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
--- src/routes/categories.router.js
+++ src/routes/categories.router.js
@@ -1,7 +1,17 @@
 const categoriesController = require("express").Router();
 
-const { getAllCategories } = require("../controllers/categories.controller");
+const {
+  getAllCategories,
+  getCategory,
+  createCategory,
+  updateCategory,
+  deleteCategory,
+} = require("../controllers/categories.controller");
 
 categoriesController.get("/", getAllCategories);
+categoriesController.get("/:id", getCategory);
+categoriesController.post("/", createCategory);
+categoriesController.patch("/:id", updateCategory);
+categoriesController.delete("/:id", deleteCategory);
 
 module.exports = categoriesController;
