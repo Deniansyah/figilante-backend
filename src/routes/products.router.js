8cff1bf5161ad4ee03f7afc4399abdb4dd80563b
MexL <madiajijah7@gmail.com>
2023-01-02 12:48:06 +0800

    done upload middleware, users, products, paymentmethods

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -7,11 +7,12 @@ const {
   updateProduct,
   deleteProduct,
 } = require("../controllers/products.controller");
+const upload = require("../middlewares/upload.middleware");
 
 productsController.get("/", getAllProducts);
 productsController.get("/:id", getProduct);
-productsController.post("/", createProduct);
-productsController.patch("/:id", updateProduct);
+productsController.post("/", upload, createProduct);
+productsController.patch("/:id", upload, updateProduct);
 productsController.delete("/:id", deleteProduct);
 
 module.exports = productsController;
