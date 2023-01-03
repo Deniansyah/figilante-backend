58f2ac232ab2003630701260d88694b2d533dbac
Rio Prayoga <yoprayogap@gmail.com>
2023-01-03 19:19:15 +0700

    add endpoint for create transaction

--- src/controllers/transactions.controller.js
+++ src/controllers/transactions.controller.js
@@ -45,3 +45,31 @@ exports.getAllTransactions = async (req, res) => {
     });
   }
 };
+
+exports.createTransactions = async (req, res) => {
+  const authorization = req.headers.authorization;
+  const token = authorization.split(" ")[1];
+  const { id } = jwt.verify(token, "backend-secret");
+
+  try {
+    const { productId, deliveryMethodId } = req.body;
+    const transaction = await prisma.transactions.create({
+      data: {
+        userId: parseInt(id),
+        productId: parseInt(productId),
+        deliveryMethodId: parseInt(deliveryMethodId),
+      },
+    });
+    return res.status(201).json({
+      success: true,
+      message: "Transaction created successfully",
+      results: transaction,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
--- src/routes/transactions.router.js
+++ src/routes/transactions.router.js
@@ -1,8 +1,10 @@
 const usersRoutes = require("express").Router();
 const {
   getAllTransactions,
+  createTransactions,
 } = require("../controllers/transactions.controller");
 
 usersRoutes.get("/", getAllTransactions);
+usersRoutes.post("/", createTransactions);
 
 module.exports = usersRoutes;
